2c73bb20538a989d76236b2ac15ae7ef1db4110f
Rishi Sharma <33762894+rishi-s8@users.noreply.github.com>
2020-11-04 21:09:36 +0530

    Backbone Code

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # book-slots
+
 The repository for the final project in Large Applications Practicum
 
 ## Group Members
@@ -12,3 +13,12 @@ C - Nivedit Reddy (B17038)
 D - Yashika Baan (B18096)
 
 E - Yogesh Deverth (B18098)
+
+## Getting Started
+
+1. Install `Mysql`.
+2. Install Requirements : `pip3 install -r requirements.txt`.
+3. Create database : `mysqladmin -u root create c4dfedBooking`.
+4. Import dump : `mysql -u root c4dfedBooking < c4dfedBooking.sql`
+5. Run App : `python3 app.py`.
+6. Visit `localhost`.
---
+++ app.py
@@ -0,0 +1,130 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.context import CryptContext
+from functools import wraps
+cryptcontext = CryptContext(schemes=["sha256_crypt", "md5_crypt", "des_crypt"])
+
+app = Flask(__name__)
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'c4dfedBooking'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# init MySQL
+mysql = MySQL(app)
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized. Please log in.', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/user/<string:username>')
+@is_logged_in
+def user(username):
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM users where username = %s", [username])
+    user = cur.fetchone()
+    cur.close()
+    return render_template('user.html', user = user)
+
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50), validators.DataRequired()])
+    username = StringField('Username', [validators.Length(min=1, max=11), validators.DataRequired()])
+    password = PasswordField('Password', [validators.Length(min=6, max=50), validators.DataRequired(), validators.EqualTo('confirm', message='Passwords Do not match')])
+    confirm = PasswordField('Confirm Password', [validators.DataRequired()])
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        username = form.username.data
+        password = cryptcontext.hash(str(form.password.data))
+
+        #Create Cursor
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, username, password) Values(%s, %s, %s)", (name, username, password))
+        mysql.connection.commit()
+
+        #Close Connection
+        cur.close()
+
+        flash("Registered Successfully", "success")
+        return redirect(url_for('index'))
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        cur = mysql.connection.cursor()
+        result = cur.execute("SELECT * FROM users where username = %s", [username])
+        if result>0:
+            data = cur.fetchone()
+            password = data['password']
+
+            if cryptcontext.verify(password_candidate, password):
+                session['logged_in'] = True
+                session['username'] = username
+                flash("You are now logged in", 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid Login'
+                return render_template('login.html', error= error)
+        else:
+            error = 'Invalid Login'
+            return render_template('login.html', error= error)
+        cur.close()
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash("You are now logged out", "success")
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM users")
+    users = cur.fetchall()
+    if result > 0:
+        return render_template('dashboard.html', users = users)
+    else:
+        msg = "No Users Found"
+        return render_template('dashboard.html', msg = msg)
+    cur.close()
+
+class BookingForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=255), validators.DataRequired()])
+    body = StringField('Body', [validators.Length(min=1), validators.DataRequired()])
+
+@app.route('/book_slot', methods=['GET', 'POST'])
+@is_logged_in
+def add_booking():
+    form = BookingForm(request.form)
+    return render_template('book_slot.html', form=form)
+
+if __name__ == '__main__':
+    app.secret_key = "8Wy@d3E&wTin"
+    app.run(debug=True)
