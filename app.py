099764122db40a57e85924cce027686b654e968b
Nivedit Reddy <39114793+Nivedit24@users.noreply.github.com>
2020-11-21 16:28:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Harrywand$24'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'c4dfedBooking'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
